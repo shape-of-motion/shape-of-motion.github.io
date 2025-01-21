463c572f9fc936b83b23982284a1adcbd142584c
FantasticOven2 <0z5e1n3g@berkeley.edu>
2025-01-21 11:22:45 -0800

    update interactive demo

---
+++ results.js
@@ -0,0 +1,260 @@
+const results = [
+  [
+    "ayuna_1",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/baby_1.viser",
+      "&initialCameraPosition=0.681,-3.638,-0.231",
+      "&initialCameraLookAt=0.055,1.067,-0.045",
+      "&initialCameraUp=-0.000,-0.120,0.993",
+    ],
+    "./data/thumbnails/baby_2.png",
+  ],
+  [
+    "sriracha-tree",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/sriracha-tree.viser",
+      "&synchronizedVideoTimeOffset=-0.05",
+      "&initialCameraPosition=0.681,-3.638,-0.231",
+      "&initialCameraLookAt=0.055,1.067,-0.045",
+      "&initialCameraUp=-0.000,-0.120,0.993",
+    ],
+    "./data/thumbnails/sriracha-tree.png",
+  ],
+  [
+    "balloon1",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/balloon1.viser",
+      "&initialCameraPosition=-0.070,-2.577,-0.124",
+      "&initialCameraLookAt=0.000,0.000,0.052",
+      "&initialCameraUp=0.000,0.000,1.000",
+    ],
+    "./data/thumbnails/baloon1.png",
+  ],
+
+  [
+    "ayuna_2",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/baby_5.viser",
+      "&initialCameraPosition=-1.204,-2.983,-0.612",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/baby.png",
+  ],
+  [
+    "balloon2",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/balloon2.viser",
+      "&initialCameraPosition=-1.054,-3.453,0.364",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000"
+    ],
+    "./data/thumbnails/balloon2.png",
+  ],
+  [
+    "handwavy",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/handwavy.viser",
+      "&initialCameraPosition=-0.538,-3.289,-0.423",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+
+    ],
+    "./data/thumbnails/handwavy.png",
+  ],
+  [
+    "spin",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/spin.viser",
+      "&initialCameraPosition=1.901,-3.222,-0.772",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/spin.png",
+  ],
+
+  [
+    "seal",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/seal.viser",
+      "&initialCameraPosition=-0.009,-3.441,0.204",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/seal.png",
+  ],
+  [
+    "cat-piano",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/cat-piano.viser",
+      "&initialCameraPosition=0.167,-3.094,-0.280",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/cat-piano.png",
+  ],
+  [
+    "dog-rotation-vase",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/dog_rotation_vase.viser",
+      "&initialCameraPosition=0.490,-2.577,-0.485",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/dog-grass.png",
+  ],
+  [
+    "mochi-high-five",
+    [
+      "https://shape-of-motion.github.io/build",
+      "/?playbackPath=https://shape-of-motion.github.io/som_viser/mochi-high-five.viser",
+      "&initialCameraPosition=0.031,-4.015,0.217",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/mochi-high-five.png",
+  ],
+  [
+    "pillow",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/pillow.viser",
+      "&initialCameraPosition=0.604,-1.987,0.046",
+      "&initialCameraLookAt=0.576,-1.890,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/pillow.png",
+  ],
+  [
+    "tiger",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/tiger.viser",
+      "&initialCameraPosition=-0.099,-4.688,0.014",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/tiger.png",
+  ],
+  [
+    "cat-spinning",
+    [
+      "https://shape-of-motion.github.io/build/",
+      "?playbackPath=https://shape-of-motion.github.io/som_viser/cat-spinning.viser",
+      "&initialCameraPosition=0.988,-7.375,-0.049",
+      "&initialCameraLookAt=0.000,0.000,0.000",
+      "&initialCameraUp=-0.000,-0.000,1.000",
+    ],
+    "./data/thumbnails/cat-spinning.png",
+  ],
+];
+
+function initializeResultSelector(resultsElement) {
+  const selectorElement = resultsElement.querySelector(".results-selector");
+  const resultsThumbnails = selectorElement.querySelector(
+    ".results-thumbnails",
+  );
+  const prevButton = selectorElement.querySelector(".results-prev");
+  const nextButton = selectorElement.querySelector(".results-next");
+  let currentIndex = 0;
+
+  function createIframe(src) {
+    const iframe = document.createElement("iframe");
+    console.log("Creating iframe with src", src);
+    iframe.src = src;
+    iframe.style.backgroundColor = "white";
+    return iframe;
+  }
+
+  function showIframe(src) {
+    const wrapper = resultsElement.querySelector(".iframe-wrapper");
+    wrapper.innerHTML = "";
+    const iframe = createIframe(Array.isArray(src) ? src.join("") : src);
+    wrapper.background = "white";
+    wrapper.appendChild(iframe);
+  }
+
+  function updateSelection(index) {
+    currentIndex = index;
+    resultsThumbnails
+      .querySelectorAll("a")
+      .forEach((a, i) =>
+        a.setAttribute("data-selected", i === index ? "true" : "false"),
+      );
+    showIframe(results[index][1]);
+    const selectedThumbnail = resultsThumbnails.children[index];
+
+    // Scroll the selected thumbnail into view
+    const thumbnailsContainer = resultsThumbnails;
+    const scrollLeft =
+      selectedThumbnail.offsetLeft -
+      (thumbnailsContainer.clientWidth - selectedThumbnail.clientWidth) / 2;
+    thumbnailsContainer.scrollTo({
+      left: scrollLeft,
+      behavior: "smooth",
+    });
+
+    // Update URL with the selected result
+    const resultName = results[index][0].toLowerCase().replace(/\s+/g, "-");
+    const currentPath = window.location.pathname;
+    const newUrl = `${currentPath}?result=${resultName}`;
+    history.pushState(null, "", newUrl);
+  }
+
+  results.forEach(([label, src, thumbnail], index) => {
+    const link = document.createElement("a");
+    link.href = "#";
+    link.setAttribute("data-selected", index === 0 ? "true" : "false");
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      updateSelection(index);
+    });
+
+    const img = document.createElement("img");
+    img.src = thumbnail;
+    img.alt =
+      "Thumbnail that can be clicked to show a result of our method: " + label;
+    img.title = label;
+
+    link.appendChild(img);
+    resultsThumbnails.appendChild(link);
+  });
+
+  prevButton.addEventListener("click", () => {
+    updateSelection((currentIndex - 1 + results.length) % results.length);
+  });
+
+  nextButton.addEventListener("click", () => {
+    updateSelection((currentIndex + 1) % results.length);
+  });
+
+  // Check URL for initial result selection
+  const urlParams = new URLSearchParams(window.location.search);
+  const initialResult = urlParams.get("result");
+  if (initialResult) {
+    const index = results.findIndex(
+      (result) =>
+        result[0].toLowerCase().replace(/\s+/g, "-") === initialResult,
+    );
+    if (index !== -1) {
+      updateSelection(index);
+    } else {
+      showIframe(results[0][1]);
+    }
+  } else {
+    showIframe(results[0][1]);
+  }
+}
+
+// Initialize all result on the page
+document.querySelectorAll(".results").forEach(initializeResultSelector);
